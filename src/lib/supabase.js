0544be2ec38b389bf9cb1a68be99e4f7406889e5
deitheramurao-coder <deitheramurao@gmail.com>
2026-05-29 04:16:58 +0800

    supabase

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,1 +1,6 @@
-import { createClient } from "@supabase/supabase-js"; console.log(import.meta.env.VITE_SUPABASE_URL); console.log(import.meta.env.VITE_SUPABASE_ANON_KEY); const supabaseUrl = import.meta.env.VITE_SUPABASE_URL; const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY; export const supabase = createClient( supabaseUrl, supabaseKey );
+import { createClient } from "@supabase/supabase-js";
+
+export const supabase = createClient(
+  import.meta.env.VITE_SUPABASE_URL,
+  import.meta.env.VITE_SUPABASE_ANON_KEY
+);
