786e1b2d6e31522af10b84fc433ec833c421bc85
deitheramurao-coder <deitheramurao@gmail.com>
2026-05-29 03:24:35 +0800

    Fix dashboard import

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import DebtorDashboard from "./DebtorDashboard";
+import DebtorDashboard from "./DebtorDashboard.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
